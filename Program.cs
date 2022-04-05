5a7a28ecbaf63c1807b8782df8c8406badd4e5cf
chakravarthy <m.chakra473@gmail.com>
2022-04-05 18:24:55 +0530

    initiated

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day16_17PracticeProb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm problems!");
+        }
+    }
+}
