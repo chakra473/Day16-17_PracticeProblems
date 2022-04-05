46bb4fc4e9c858317a15057ae30bb4255b12308c
chakravarthy <m.chakra473@gmail.com>
2022-04-05 18:33:21 +0530

    added[bubbleSort file]

---
+++ Algorithm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16_17PracticeProb
+{
+    internal class Algorithm
+    {
+        public void bubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        // swap temp and arr[i]
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        // Prints the array
+        public void printArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ namespace Day16_17PracticeProb
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Algorithm problems!");
+            int[] arr = { 32, 45, 67, 2, 56, 78, 1, 54, 7 };
+            Console.WriteLine("before sorted");
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Algorithm obj = new Algorithm();
+            obj.bubbleSort(arr);
+            Console.WriteLine();
+            Console.WriteLine("Sorted array");
+            obj.printArray(arr);
         }
     }
 }
